15c8a31218014be6b82ea72fd823f7d3cd847123
david toska <david.toska@outlook.com>
2018-10-18 18:47:36 +0200

    Added EventList, and Event form, and some css to the project.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import "assert";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/eventDashBoard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        app
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
